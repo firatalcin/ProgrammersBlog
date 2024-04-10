bf4243450ce178fc992da763d8ca57c999e501c5
Fırat Alçın <firatalcin04@gmail.com>
2024-04-10 22:51:20 +0300

    Solution ve Proje Oluşturuldu

---
+++ ProgrammersBlog/ProgrammersBlog.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgrammersBlog.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
