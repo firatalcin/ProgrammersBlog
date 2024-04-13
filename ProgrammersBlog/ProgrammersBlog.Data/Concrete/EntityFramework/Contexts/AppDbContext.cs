1bbc170482bf00dd080ade28563b436df47f794a
Fırat Alçın <firatalcin04@gmail.com>
2024-04-13 15:09:29 +0300

    User Sınıfının Mapping İşlemleri Oluşturuldu

--- ProgrammersBlog/ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/AppDbContext.cs
+++ ProgrammersBlog/ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Mappings;
 using ProgrammersBlog.Entities.Concrete;
 
 namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
@@ -15,5 +16,11 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new ArticleMap());
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ArticleMap).Assembly);
+        }
     }
 }
