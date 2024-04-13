1466256cf40b4dd438c74c2803a79913b3346145
Fırat Alçın <firatalcin04@gmail.com>
2024-04-13 14:58:00 +0300

    Role Sınıfının Mapping İşlemleri Oluşturuldu

--- ProgrammersBlog/ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
+++ ProgrammersBlog/ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
@@ -8,7 +8,23 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
     {
         public void Configure(EntityTypeBuilder<Role> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(30);
+            builder.Property(x => x.Description).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(250);
+
+            builder.Property(x => x.CreatedByName).IsRequired();
+            builder.Property(x => x.CreatedByName).HasMaxLength(50);
+            builder.Property(x => x.CreatedDate).IsRequired();
+            builder.Property(x => x.ModifiedByName).IsRequired();
+            builder.Property(x => x.ModifiedByName).HasMaxLength(50);
+            builder.Property(x => x.ModifiedDate).IsRequired();
+            builder.Property(x => x.IsActive).IsRequired();
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.Note).IsRequired();
+            builder.ToTable("Roles");
         }
     }
 }
