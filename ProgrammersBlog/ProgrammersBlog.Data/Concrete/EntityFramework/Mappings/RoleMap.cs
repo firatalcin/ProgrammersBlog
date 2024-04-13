7294abcff1550e73aa999592548a1bf12365e3bc
Fırat Alçın <firatalcin04@gmail.com>
2024-04-13 14:54:47 +0300

    Comment Sınıfının Mapping İşlemlerinin Oluşturulması

---
+++ ProgrammersBlog/ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProgrammersBlog/ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/UserMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
